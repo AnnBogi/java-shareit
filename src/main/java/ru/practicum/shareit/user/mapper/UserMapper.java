67a74fd594d31a4f426e21d0a599f8bd48616c8d
Bogomolova <BatAnn04@yandex.ru>
2023-08-08 02:31:26 +0300

    V 1.0

---
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.user.mapper;
+
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public static UserDto toUserDto(User user) {
+        return UserDto.builder()
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+
+    public static User toUser(UserDto userDto) {
+        return User.builder()
+                .name(userDto.getName())
+                .email(userDto.getEmail())
+                .build();
+    }
+}
