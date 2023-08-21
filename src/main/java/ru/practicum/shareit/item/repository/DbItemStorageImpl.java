ed45d528d014da4b54a958fcbb8a0a221d3e3e59
Bogomolova <BatAnn04@yandex.ru>
2023-08-22 00:14:12 +0300

    V 2.03

--- src/main/java/ru/practicum/shareit/item/repository/DbItemStorageImpl.java
+++ src/main/java/ru/practicum/shareit/item/repository/DbItemStorageImpl.java
@@ -66,7 +66,7 @@ public class DbItemStorageImpl implements ItemStorage {
     public Item updateItem(Long userId, Long itemId, Item item) {
         Item updatedItem = getItem(itemId);
 
-        if (!Objects.equals(updatedItem.getOwner().getId(), userId)) {
+        if (!updatedItem.getOwner().getId().equals(userId)) {
             throw new ObjectNotFoundException("User don't have access to this item.");
         }
 
