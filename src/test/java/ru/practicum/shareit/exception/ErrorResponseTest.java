d48c1c19291a96490bd1d2d1bf3444555eb6b9be
Bogomolova <BatAnn04@yandex.ru>
2023-09-18 00:24:04 +0300

    V.1.0

--- src/test/java/ru/practicum/shareit/exception/ErrorResponseTest.java
+++ src/test/java/ru/practicum/shareit/exception/ErrorResponseTest.java
@@ -1,2 +1,14 @@
-package ru.practicum.shareit.exception;public class ErrorResponseTest {
+package ru.practicum.shareit.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ErrorResponseTest {
+
+    @Test
+    void getError() {
+        ErrorResponse errorResponse = new ErrorResponse("ERROR");
+        assertEquals(errorResponse.getError(), "ERROR");
+    }
 }
